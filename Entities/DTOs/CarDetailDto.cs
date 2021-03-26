3c79015bc86c19bd5ce2cd05ecb14710b2eb6c55
sanlimamy <69751354+sanlimamy@users.noreply.github.com>
2021-03-26 13:12:12 +0300

    3/26/21

--- Entities/DTOs/CarDetailDto.cs
+++ Entities/DTOs/CarDetailDto.cs
@@ -10,8 +10,9 @@ namespace Entities.DTOs
         public string CarName { get; set; }
         public string BrandName { get; set; }
         public string ColorName { get; set; }
+        public int DailyPrice { get; set; }
 
         //public string CarImage { get; set; }
-        public int DailyPrice { get; set; }
+
     }
 }
